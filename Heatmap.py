78fa788688aa4c94b0f4724e87b48261f1744bf5
kslangeland <32144253+kslangeland@users.noreply.github.com>
2020-09-14 05:55:39 -0700

    Update Heatmap.py

--- Heatmap.py
+++ Heatmap.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 df = pandas.read_csv("results.csv")
 print(df)
-df = df.pivot("Blue Signal Cost", "Weapon Value", "Prob Red Accepts Given Blue Signals")
+df = df.pivot("Blue Signal Cost", "Weapon Value", "Prob Red Develops Given Blue Signals")
 print(df)
 hm = seaborn.heatmap(df, annot = True)
 plt.savefig("Heatmap_RedBehavior.png")
